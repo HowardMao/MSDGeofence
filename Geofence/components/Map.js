bfdfdfba794e4e0d0d696be3131c0180b6b6414e
Howard Mao <howardmao333@gmail.com>
2021-10-20 15:27:00 +1300

    re-added Geofence

---
+++ Geofence/components/Map.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { View, StyleSheet } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+
+export default class Map extends Component {
+  state = {
+    latitude: -36.85281,
+    longitude: 174.76696,
+    latitudeDelta: 0.005,
+    longitudeDelta: 0.005,
+    circleRadius: 20,
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          provider={PROVIDER_GOOGLE}
+          showsUserLocation={true}
+          initialRegion={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            latitudeDelta: this.state.latitudeDelta,
+            longitudeDelta: this.state.longitudeDelta,
+          }}
+        >
+          <MapView.Circle
+            key={2}
+            center={{
+              latitude: this.state.latitude,
+              longitude: this.state.longitude,
+            }}
+            radius={this.state.circleRadius}
+          />
+        </MapView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  map: {
+    flex: 1,
+  },
+});
